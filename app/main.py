bba2d38b76f2995ea21dd5c3882dde39fbea2af1
steventejeda <steventejeda@gmail.com>
2023-07-12 23:14:03 -0400

    stage 2

--- app/main.py
+++ app/main.py
@@ -8,11 +8,14 @@ def main():
     pong = "+PONG\r\n"
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()
 
-    with conn: 
-        conn.recv(1024)
-        conn.send(pong.encode())
+
+    while True:
+        conn, addr = server_socket.accept()
+
+        with conn: 
+            conn.recv(1024)
+            conn.send(pong.encode())
 
 
 if __name__ == "__main__":
