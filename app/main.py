c0411344ed9afdc48be3257fd37aa7bf40cadfc1
steventejeda <steventejeda@gmail.com>
2023-07-15 10:07:56 -0400

    Respond to multiple pings

--- app/main.py
+++ app/main.py
@@ -6,16 +6,16 @@ def main():
     print("Logs from your program will appear here!")
 
     pong = "+PONG\r\n"
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    conn, addr = server_socket.accept()
 
-
-    while True:
-        conn, addr = server_socket.accept()
-
-        with conn: 
-            conn.recv(1024)
-            conn.send(pong.encode())
+    with server_socket:
+        with conn:
+            while True: 
+                data = conn.recv(1024)
+                if not data: 
+                    break
+                conn.send(pong.encode())
 
 
 if __name__ == "__main__":
