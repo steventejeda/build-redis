793d44c1bff7503f09159a9b44839300271d4dc4
steventejeda <steventejeda@gmail.com>
2023-07-16 20:55:01 -0400

    handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,21 +1,29 @@
 # Uncomment this to pass the first stage
 import socket
+from concurrent.futures import ThreadPoolExecutor
+
+def reply(connection):
+    pong = "+PONG\r\n"
+    utf = "utf-8"
+
+    while True:
+        if not connection.recv(1024):
+            break
+        connection.send(bytes(pong, utf))
+    print("Client disconnected")
 
 
 def main():
     print("Logs from your program will appear here!")
 
-    pong = "+PONG\r\n"
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, address = server_socket.accept()
+    thread_pool_executor = ThreadPoolExecutor(max_workers=3)
 
-    with server_socket:
-        with connection:
-            while True: 
-                data = connection.recv(1024)
-                if not data: 
-                    break
-                connection.send(pong.encode())
+    while True:
+        connection, address = server_socket.accept()
+        thread_pool_executor.submit(reply, connection)
+        
+
 
 
 if __name__ == "__main__":
